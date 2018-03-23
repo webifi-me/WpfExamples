1834dfa61456ed897216e8f45168056ac60b49ec
Hubert Jetschko <hubert@robospace.co.nz>
2018-03-23 13:33:08 +1300

    + First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# WPF Examples
+
+This project shows how to use our Webifi and Serial Port WPF controls. Documentation for these controls can be viewed at:
+https://www.webifi.me/cs-library-documentation/
---
+++ WpfExampleWebifi/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WebifiLib;
+
+namespace WpfExamples
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void webifi_DataReceived(object sender, byte[] data, string dataType, int fromSessionID)
+        {
+            string text = Webifi.ConvertByteArrayToString(data)
+                + " Data type: " + dataType + " SID: " + fromSessionID.ToString() + "\n";
+            textboxRecData.Text += text;
+        }
+
+        private void buttonClearRecData_Click(object sender, RoutedEventArgs e)
+        {
+            textboxRecData.Text = "";
+        }
+
+        private void buttonSendData_Click(object sender, RoutedEventArgs e)
+        {
+            WebifiDataToSend dataToSend = new WebifiDataToSend();
+            dataToSend.dataForSending = Webifi.ConvertStringToByteArray(textboxSendData.Text);
+            webifiControl.SendData(dataToSend);
+        }
+    }
+}
